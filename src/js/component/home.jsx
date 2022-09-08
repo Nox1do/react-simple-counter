732f11d21763379e8bfc85bae81eb6f93d5b25ab
Nox1do <noogux@gmail.com>
2022-09-08 06:25:46 +0000

    added fontAwesome icon to the UI

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faClock} from "@fortawesome/free-solid-svg-icons"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,7 +10,7 @@ function Home(props) {
 	return (
 <div className='App'>
 	<div className="bigCounter">
-		<div className="calendar">1</div>
+		<div className="calendar"><FontAwesomeIcon icon={faClock}		></FontAwesomeIcon></div>
 		<div className="four">{props.digitFour %10}</div>
 		<div className="three">{props.digitThree %10}</div>
 		<div className="two">{props.digitTwo %10}</div>
