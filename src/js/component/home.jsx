08d8bec51309ea76910fc4d3d537cf722e680b98
Nox1do <noogux@gmail.com>
2022-09-08 06:29:23 +0000

    commenting new files

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,12 +9,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 function Home(props) {
 	return (
 <div className='App'>
-	<div className="bigCounter">
-		<div className="calendar"><FontAwesomeIcon icon={faClock}		></FontAwesomeIcon></div>
-		<div className="four">{props.digitFour %10}</div>
-		<div className="three">{props.digitThree %10}</div>
-		<div className="two">{props.digitTwo %10}</div>
-		<div className="one">{props.digitOne %10}</div>
+	<div className="bigCounter"> //contenedor principal
+		<div className="calendar"> //icono 
+		<FontAwesomeIcon icon={faClock}></FontAwesomeIcon></div> 
+		<div className="four">{props.digitFour %10}</div> //digito 4
+		<div className="three">{props.digitThree %10}</div> //digito 3
+		<div className="two">{props.digitTwo %10}</div>  //digito 2
+		<div className="one">{props.digitOne %10}</div> // digito 1
 	  </div>
 </div>
 	);
