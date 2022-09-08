2dd7f6277c49eb781b6f8bb615e54fd5255a0d9d
Nox1do <noogux@gmail.com>
2022-09-08 06:06:04 +0000

    updating the simple counter 1st module

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,17 +6,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 function Home(props) {
 	return (
-	  <div className='App'>
-		<div><p>🕛</p></div>
-		<div><p>0</p></div>
-		<div><p>0</p></div>
-		<div><p>0</p></div>
-		<div><p>0</p></div>
-		<div><p>0</p></div>
-		<div><p className="counter">0</p></div>
+<div className='App'>
+	<div className="bigCounter">
+		<div className="calendar">1</div>
+		<div className="four">{props.digitFour %10}</div>
+		<div className="three">{props.digitThree %10}</div>
+		<div className="two">{props.digitTwo %10}</div>
+		<div className="one">{props.digitOne %10}</div>
 	  </div>
+</div>
 	);
   };
+
 
 export default Home;
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -8,5 +9,33 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+//propTypes
+Home.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+};
+
+//counter code here
+let counter = 0;
+setInterval(function(){
+const four = Math.floor(counter/10000)
+const three = Math.floor(counter/1000)
+const two = Math.floor(counter/10)
+const one = Math.floor(counter/1)
+counter++;
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Home
+    digitOne ={one}
+    digitTwo ={two}
+    digitThree ={three}
+    digitFour= {four}
+    
+    />, document.querySelector("#app"));
+},1000)
+
+
+
+
+
